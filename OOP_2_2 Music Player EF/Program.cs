a41ba53d1dd1fc428258270de2dd49901fef23e6
Denis Kochetkov <deniskochetkov1@gmail.com>
2019-04-17 21:45:23 +0300

    Добавьте файлы проекта.

---
+++ OOP_2_2 Music Player EF/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_2_2_Music_Player_EF
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
